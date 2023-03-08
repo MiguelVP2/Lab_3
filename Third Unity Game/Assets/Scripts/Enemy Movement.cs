3595d664c06abba510cad746caa0449195ff5468
Miguel Velazquez <VelazquezMM@hendrix.edu>
2023-03-07 23:03:32 -0600

    new scripts, cat is now prefab

--- Third Unity Game/Assets/Scripts/Enemy Movement.cs
+++ Third Unity Game/Assets/Scripts/Enemy Movement.cs
@@ -4,11 +4,62 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public List<GameObject> locs;
+    private Queue<GameObject> qlocs;
+
+    public float duration = 5;
+    
+    public string text;
     // Start is called before the first frame update
     void Start()
     {
+         qlocs = new Queue<GameObject>();
+        foreach (GameObject go in locs)
+        {
+            qlocs.Enqueue(go);
+        }
+        NextUp();
         
     }
+
+    void NextUp()
+    {
+        GameObject pong = qlocs.Dequeue();
+        StartCoroutine(LerpPosition(pong.transform.position));
+        qlocs.Enqueue(pong);
+    }
+
+     public void StopMoving(){
+        StopAllCoroutines();
+    }
+
+    public void StartMoving(){
+        Start();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider2D) {
+        print("Found..");
+        if (collider2D.gameObject.CompareTag("Player")) {
+            //GameManager.Instance.DialogShow(text);
+            GameManager.Instance.StopGame();
+        }
+    }
+
+    IEnumerator LerpPosition(Vector2 targetPosition)
+    {
+        float time = 0;
+        Vector2 startPosition = transform.position;
+
+        while (time < duration)
+        {
+            transform.position = Vector2.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+        NextUp();
+    }
+
 
     // Update is called once per frame
     void Update()
---
+++ Third Unity Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance {get; private set;}
+    public GameObject cat;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public Vector2 getPos(){
+        print("gamemanager called");
+        positionNumber += 1;
+        return posList[(positionNumber - 1)];
+    }
+
+    public void StopGame(){
+        npc.GetComponent<EnemyMovement>().StopMoving();
+        player.GetComponent<Player>().StopMoving();
+    }
+    
+    void Start()
+    {
+        
+    }
+
+}
